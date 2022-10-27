76cb0d97fdecf37e5f03f20984b0af0ab7ced0bb
Frantisek <frantisek@craftcoders.cz>
2022-10-27 14:53:38 +0200

    Add basic server for client

---
+++ server/README.md
@@ -0,0 +1,11 @@
+# API Server
+
+To run backend server:
+
+```shell
+yarn install
+yarn server
+```
+
+## Tasks
+1) Implement custom endpoint for marking single todo item as "done". Calling this endpoint sets `done` field to `true` and `finishedAt` field sets to current time. Use this new endpoint on client 
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  next();
+});
+
+// Use default router
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
