67c3f575ed7ecdb26543e1cd8719ff24cb0420bb
zlatka-n <zla.b.ngu@gmail.com>
2022-10-28 16:30:09 +0200

    call getTodoItems and map list items

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,37 @@ import { List } from "./components/List";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { ThemeProvider } from "./components/ThemeProvider";
+import { useEffect, useMemo, useState } from 'react';
+import { getTodoItems } from "./api/axios";
+import { TodoItems } from "./types";
+import { ListItem } from "./components/ListItem";
 
-export const App: React.FC = () => (
-    <ThemeProvider>
-        <Container>
-            <Layout>
-                <Header handleAddItem={() => console.warn("unimplemented")}>To Do app</Header>
-                <List />
-                <Footer />
-            </Layout>
-        </Container>
-    </ThemeProvider>
-);
+export const App: React.FC = () => {
+    const [todos, setTodos] = useState<TodoItems>()
+
+    useEffect(() => {
+        getTodoItems().then(response => setTodos(response))
+    }, [])
+
+    const numberOfDone = useMemo(() => todos?.filter(item => item.done).length, [todos])
+
+    const numberOfTodo = useMemo(() => {
+        if (!todos|| !numberOfDone) return
+
+        return todos?.length - numberOfDone
+    }, [todos])
+
+    return (
+        <ThemeProvider>
+            <Container>
+                <Layout>
+                    <Header handleAddItem={() => console.warn("unimplemented")}>To Do app</Header>
+                    <List>
+                        {todos?.map(item => <ListItem label={item.title} key={item.id} checked={item.done} handleEdit={() => alert('todo edit')}  handleRemoval={() => alert('todo remove')}/>)}
+                    </List>
+                    <Footer todoItems={numberOfTodo} doneItems={numberOfDone}/>
+                </Layout>
+            </Container>
+        </ThemeProvider>
+    )
+}
