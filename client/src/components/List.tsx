6d3a21151823c505bbdc5de82f6f78e4b6b985b1
zlatka-n <zla.b.ngu@gmail.com>
2022-10-28 16:58:01 +0200

    add min-height for List - Footer is in the bottom

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 const StyledDiv = styled.div`
     display: flex;
     flex-direction: column;
+    min-height: 50vh;
+    background-color: yellow;
 `;
 
 export const List: React.FC<PropsWithChildren> = ({ children }) => <StyledDiv>{children}</StyledDiv>;
