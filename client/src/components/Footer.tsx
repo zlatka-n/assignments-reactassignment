c1e7fee80c22a73cb91fe3c0d0786f1c802e4d68
zlatka-n <zla.b.ngu@gmail.com>
2022-10-29 16:22:16 +0200

    fix styling for footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,11 +3,18 @@ import styled from "styled-components";
 
 const StyledDiv = styled.footer`
     display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
     margin-top: 15px;
     padding-top: 15px;
     border-top: 1px solid;
-    border-color: ${(props) => props.theme.colors.olive6}; ;
+    border-color: ${(props) => props.theme.colors.olive6};
 `;
+
+const StyledText = styled.span`
+     font-weight: 700;
+`
 
 export type FooterProps = {
     todoItems?: number;
@@ -16,7 +23,7 @@ export type FooterProps = {
 export const Footer: React.FC<FooterProps> = ({ todoItems, doneItems }) => (
     
     <StyledDiv>
-        Todo: {todoItems ? todoItems : 0}
-        Done: {doneItems ? doneItems : 0}
+        <p>Todo: <StyledText>{todoItems ? todoItems : 0}</StyledText></p>
+        <p>Done: <StyledText>{doneItems ? doneItems : 0}</StyledText></p>
     </StyledDiv>
 );
