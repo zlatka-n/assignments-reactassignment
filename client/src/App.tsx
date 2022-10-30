07537a611a934dbb639c8ce0e91ac50787951425
zlatka-n <zla.b.ngu@gmail.com>
2022-10-30 08:21:14 +0100

    remove useMemo for primitive values

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ import { ListItem } from "./components/ListItem";
 import { useHandleOpen } from "./hooks/useHandleOpen";
 
 export const App: React.FC = () => {
-    const [todos, setTodos] = useState<TodoItems>()
+    const [todos, setTodos] = useState<TodoItems>([])
 
     useEffect(() => {
     
@@ -21,16 +21,11 @@ export const App: React.FC = () => {
             })
         },2000)
        
-        return() => clearInterval(interval)
+        return () => clearInterval(interval)
     }, [])
 
-    const numberOfDone = useMemo(() => todos?.filter(item => item.done).length, [todos])
-
-    const numberOfTodo = useMemo(() => {
-        if (!todos || !numberOfDone) return
-
-        return todos?.length - numberOfDone
-    }, [todos])
+    const numberOfDone = todos?.filter(item => item.done).length
+    const numberOfTodos = (todos?.length && numberOfDone) ? todos?.length - numberOfDone : todos?.length
 
     const sortedTodos = useMemo(() => todos?.sort((x, y) => (x.done === y.done)? 0 : x.done? 1 : -1 && y.createdAt - x.createdAt), [todos])
 
@@ -42,7 +37,7 @@ export const App: React.FC = () => {
                     <List>
                         {sortedTodos?.map(item => <ListItem label={item.title} key={item.id} todoId={item.id} checked={item.done} handleEdit={() => alert('todo edit')}  handleRemoval={() => alert('todo remove')}/>)}
                     </List>
-                    <Footer todoItems={numberOfTodo} doneItems={numberOfDone}/>
+                    <Footer todoItems={numberOfTodos} doneItems={numberOfDone}/>
                 </Layout>
             </Container>
         </ThemeProvider>
